2fac1cb47b714c0d715957763717946fb44ec15e
Natalija <93840667+natalijapav@users.noreply.github.com>
2022-01-03 12:12:33 +0100

    Update

--- test/unit.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert');
-const MyMath = require('../Maths');
-
-describe('Maths test', function() {
-    it('tEST IF 2-1=1', function() {
-       assert.equal(MyMath.sub(2,1), 1);
-       
-    })
- });
