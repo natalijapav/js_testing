052e9554e4821f463ba6d118c0956ae59ee778b5
Natalija <93840667+natalijapav@users.noreply.github.com>
2022-01-03 12:41:03 +0100

    testiranje

---
+++ testing2/demo.test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { Builder, By, until, Key } = require('selenium-webdriver');
+const chai = require('chai');
+const expect = chai.expect;
+const chrome = require('selenium-webdriver/chrome');
+
+describe('Demo QA Tests', function() {
+    let driver;
+
+    before(async function() {
+        let service = new chrome.ServiceBuilder('C:\\Users\\natal\\testing\\chromedriver.exe').build()
+        chrome.setDefaultService(service);
+
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    after(function() {
+        return driver.quit();
+    });
+
+    
+
+
+
+
+
+
+});    
